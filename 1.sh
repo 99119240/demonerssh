91ed680432bd377154ecfa0df50674a31a7f029d
99119240 <105958391+99119240@users.noreply.github.com>
2022-09-16 11:16:04 -0600

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,8 @@
+echo ERROR 303 Could not find package .filed-server.py
+echo DO NOT exit
+sleep 4
+echo jk here your stuff!
+sleep 0.6
+echo @C7 Ez server
+echo Pass = ligma.com
+ssh root@139.144.18.37 ligma.com
